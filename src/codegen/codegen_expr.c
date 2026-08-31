6c226add1b24474ba07c472b99b6cc35162979dd
shv-ng <shivangsrivastava157@gmail.com>
2026-08-31 19:04:44 +0530

    feat(codegen): implement LLVM codegen for binary expressions

--- src/codegen/codegen_expr.c
+++ src/codegen/codegen_expr.c
@@ -6,6 +6,35 @@ LLVMValueRef codegen_int_literal(struct CodegenCtx *ctx, struct AstNode *node) {
   return LLVMConstInt(t, (unsigned long long)node->as.int_literal.value, 1);
 }
 
+LLVMValueRef codegen_binary_expr(struct CodegenCtx *ctx, struct AstNode *node) {
+  LLVMValueRef left_val = codegen_expr(ctx, node->as.binary_expr.left);
+  LLVMValueRef right_val = codegen_expr(ctx, node->as.binary_expr.right);
+
+  switch (node->as.binary_expr.op) {
+  case OP_ADD:
+    return LLVMBuildAdd(ctx->builder, left_val, right_val, "add");
+  case OP_SUB:
+    return LLVMBuildSub(ctx->builder, left_val, right_val, "sub");
+  case OP_MULIPLY:
+    return LLVMBuildMul(ctx->builder, left_val, right_val, "mul");
+  case OP_DIVIDE:
+    return LLVMBuildSDiv(ctx->builder, left_val, right_val, "div");
+  case OP_MODULO:
+    return LLVMBuildSRem(ctx->builder, left_val, right_val, "mod");
+  case OP_LEFT_SHIFT:
+    return LLVMBuildShl(ctx->builder, left_val, right_val, "left_shift");
+  case OP_RIGHT_SHIFT:
+    return LLVMBuildLShr(ctx->builder, left_val, right_val, "right_shift");
+  case OP_BITWISE_XOR:
+    return LLVMBuildXor(ctx->builder, left_val, right_val, "xor");
+  case OP_BITWISE_OR:
+    return LLVMBuildOr(ctx->builder, left_val, right_val, "or");
+  case OP_BITWISE_AND:
+    return LLVMBuildOr(ctx->builder, left_val, right_val, "and");
+  default:
+    return NULL;
+  }
+}
 LLVMValueRef codegen_unary_expr(struct CodegenCtx *ctx, struct AstNode *node) {
   LLVMValueRef val = codegen_expr(ctx, node->as.unary_expr.expr);
   switch (node->as.unary_expr.op) {
@@ -24,8 +53,12 @@ LLVMValueRef codegen_expr(struct CodegenCtx *ctx, struct AstNode *node) {
   switch (node->kind) {
   case AST_INT_LITERAL:
     return codegen_int_literal(ctx, node);
+
   case AST_UNARY_EXPR:
     return codegen_unary_expr(ctx, node);
+
+  case AST_BINARY_EXPR:
+    return codegen_binary_expr(ctx, node);
 
   default:
     ctx->error_count += 1;
